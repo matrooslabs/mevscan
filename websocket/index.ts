e75ffe080148b003dfdbf2578362af3dab1e0065
Chee Chyuan Ang <cheechyuanang@Chees-MacBook-Pro-2.local>
2025-11-27 17:56:34 +0800

    add userid, add test publish

--- websocket/index.ts
+++ websocket/index.ts
@@ -8,5 +8,15 @@ const pubnub = new PubNub({
     subscribeKey: process.env.PUBNUB_SUBSCRIBE_KEY || "",
     publishKey: process.env.PUBNUB_PUBLISH_KEY || "",
     secretKey: process.env.PUBNUB_SECRET_KEY || "",
+    userId: process.env.PUBNUB_USER_ID || ""
 });
 
+
+pubnub.publish({
+    channel: "test",
+    message: { "test": "hello" }
+},
+    (status, response) => {
+        console.log(status);
+        console.log(response);
+    })
