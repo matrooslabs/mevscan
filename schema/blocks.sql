e5c74a34b7c88b56ab1a46ae4a0481484035c795
Jinsuk Park <totorovirus@gmail.com>
2025-12-18 13:40:32 +0900

    feat(schema): add projection to blocks table

--- schema/blocks.sql
+++ schema/blocks.sql
@@ -1,7 +1,19 @@
-CREATE TABLE IF NOT EXISTS ethereum.blocks (
+CREATE TABLE IF NOT EXISTS ethereum.blocks
+(
     `block_number` UInt64,
     `block_hash` String,
     `block_timestamp` UInt64,
     `valid` UInt8,
-) ENGINE = MergeTree()
+
+    PROJECTION proj_by_block_number
+    (
+        SELECT
+            block_number,
+            block_timestamp,
+            block_hash,
+            valid
+        ORDER BY block_number
+    )
+)
+ENGINE = MergeTree()
 ORDER BY (block_timestamp, block_number)
